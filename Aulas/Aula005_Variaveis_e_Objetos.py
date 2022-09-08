2b84d58a19ead6b13fa347952277a01e28640e0e
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-09-08 00:48:35 -0300

    Initial commit

---
+++ Aulas/Aula005_Variaveis_e_Objetos.py
@@ -0,0 +1,24 @@
+x = 1
+print(x)
+y = 3.13
+print(y)
+w = True
+z = False
+print(z)
+
+
+print("Esse valor será impresso no console")
+
+print("Texto e duas variáveis", x, ",", y)
+
+insira = input("Insira um valor: ")
+print(insira)
+
+print(type(insira)) # Verifica o tipo de dado na minha variável
+
+"""
+Conversão de valores
+"""
+
+insira = float(insira) # Convertendo para tipo float
+print(type(insira))
