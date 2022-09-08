7224abdbbd5dc2bb398b7236b8126d0c7a3e9765
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-09-08 19:47:44 -0300

    Initial commit

--- Aulas_PySpark/Aula001_Import_PySpark.py
+++ Aulas_PySpark/Aula001_Import_PySpark.py
@@ -1,8 +1,47 @@
-from pyspark.sql import SparkSession  #Na cor cinza significa que não estamos utilizando
+# from pyspark.sql import SparkSession  #Na cor cinza significa que não estamos utilizando
 
+"""
 def main():
     pass
 
 if __name__ == "__main__":
     main()
+"""
 
+"""
+# Primeira forma
+# [] definir quantas treads - * = 1 ou mais
+# definindo o appName
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("estudando-dataframes") \
+        .getOrCreate()
+
+    print(spark)
+"""
+
+
+# Segunda forma
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    conf = SparkConf() \
+    .setMaster("local") \
+    .setAppName("estudando-dataframes")
+
+    spark = SparkSession.builder.config(conf = conf).getOrCreate()
+
+df = (spark
+      .read
+      .format("csv")
+      .option("header","true")
+      .option("inferSchema","true")
+      .option("delimiter",",")
+      .load("csv/cientista_de_dados.csv"))
+
+df.show(100)
