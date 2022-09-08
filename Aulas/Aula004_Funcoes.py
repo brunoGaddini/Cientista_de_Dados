ce463e800d931c1a97fd29fa244d12f6cca46130
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-09-08 00:02:41 -0300

    Initial commit

---
+++ Aulas/Aula004_Funcoes.py
@@ -0,0 +1,9 @@
+# O principio de uma função é utilizar o def(), isso diz ao py que estou usando uma função. Sempre utilizar a identação
+
+#Ex:
+
+def imprime():
+    print("esta é uma função")
+
+imprime()
+imprime()
