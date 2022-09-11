218d2119c8080895d92c58e6962851b12c510c86
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-09-11 00:56:43 -0300

    Initial commit

---
+++ Aulas/Aula010_Numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Biblioteca muito utilizada para tratar dados
+
+""" Criando uma matriz unidimensional"""
+
+#() é um parametro do método array do numpy
+#[] é o vetor de dados
+
+matriz_uni = np.array([12,34,26,18,10])
+print(matriz_uni)
+print(type(matriz_uni))
+
+"""
+Criando uma array de tipo específico
+Criando uma array como float de 64bits
+"""
+
+
+
